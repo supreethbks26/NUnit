4e37fa1efcded5866eb8e98806fb80f558400c5e
Supreeth kundapura shashikiran <subharad@microsoft.com>
2019-05-16 12:46:55 +0530

    added mul test

---
+++ Calc.Test/MulTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc.Test
+{
+    [TestFixture]
+    public class MulTest
+    {
+        [TestCase(0,0,0)]
+        [TestCase(100,10,10)]
+        [Test]
+
+        public void shouldmul(int ans, int n1, int n2)
+        {
+            Icalc c = new NUnit.Calc();
+            Assert.AreEqual(ans, c.mul(n1, n2));
+        }
+
+
+    }
+}
